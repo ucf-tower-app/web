101e24e335e2a9d0d81c653ec8d42915ee8513d8
Jacob Steinebronn <jakesteinebronn@gmail.com>
2022-11-24 00:35:14 +0000

    Testing for user creation

--- src/pages/BackendTesting.tsx
+++ src/pages/BackendTesting.tsx
@@ -3,43 +3,41 @@ import logo from '../logo.svg';
 import '../App.css';
 import { Page } from '../App';
 
-import { getAuthUser, makeAuthUser, sendAuthEmail, signIn, testConnection } from '../xplat/api'
+import { createUser, getCurrentUser, sendAuthEmail, signIn } from '../xplat/api'
+import { auth } from '../xplat/Firebase';
 
 const BackendTesting = ({ setCurrentPage }: { setCurrentPage: (arg0: Page) => void; }) => {
-  const [email, onChangeUsername] = React.useState("email");
+  const [email, onChangeEmail] = React.useState("email");
+  const [username, onChangeUsername] = React.useState("username");
   const [password, onChangePassword] = React.useState("password");
 
-  const makeUser = () => {
-    makeAuthUser(email, password)
+  async function makeUser() {
+    await createUser(email, password, username);
     console.log("done")
-    setCurrentPage(Page.BackendTesting);
+    console.log(await getCurrentUser())
+  }
+  
+  async function signin() {
+    await signIn(email, password);
+    const user = await getCurrentUser();
+    console.log(await user.getUsername());
+    console.log(await user.getStatus());
   }
 
-  const signin = () => {
-    signIn(email, password)
-  }
-
-  const printuser = () => {
-    console.log(getAuthUser())
-  }
-
+  const _onChangeEmail = (evt: { target: { value: React.SetStateAction<string>; }; }) => { onChangeEmail(evt.target.value) }
   const _onChangeUsername = (evt: { target: { value: React.SetStateAction<string>; }; }) => { onChangeUsername(evt.target.value) }
   const _onChangePassword = (evt: { target: { value: React.SetStateAction<string>; }; }) => { onChangePassword(evt.target.value) }
 
-
-  const printhref = () => { console.log(window.location.href) }
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <button onClick={testConnection}>Test user get</button>
-        <button onClick={printuser}>Print auth user</button>
-        <input type="text" value={email} onChange={_onChangeUsername} />
+        <input type="text" value={email} onChange={_onChangeEmail} />
+        <input type="text" value={username} onChange={_onChangeUsername} />
         <input type="text" value={password} onChange={_onChangePassword} />
         <button onClick={makeUser}>Make a user  </button>
         <button onClick={signin}>Sign in </button>
         <button onClick={sendAuthEmail}>Send the email  </button>
-        <button onClick={printhref}>Href?  </button>
         <a
           className="App-link"
           href="https://reactjs.org"
