5b13243e667029d086cc4b2115560923c191ca7d
AlexRutledge1 <31017536+AlexRutledge1@users.noreply.github.com>
2023-01-11 10:40:58 -0500

    Create AuthorHandle.tsx
    Created a component for author handles (used both in posts & comments)

---
+++ src/components/AuthorHandle.tsx
@@ -0,0 +1,33 @@
+import { useState, useEffect } from 'react';
+import { User } from '../xplat/types/user';
+import { HStack, Text, Pressable, Skeleton } from 'native-base';
+import placeholder_image from '../placeholder_image.jpg';
+import './css/feed.css';
+
+const AuthorHandle = ({author}: {author: User | undefined}) => {
+    // TODO: Implement clickable handle that will direct to author profile
+    const [name, setName] = useState('');
+    const [avatarURL, setAvatarURL] = useState<string | undefined>();
+    const [isLoaded, setIsLoaded] = useState(false);
+    
+    useEffect( () => {
+        const getData = async () => {
+            await author?.getData();
+            author?.getUsername().then(setName);
+            author?.getAvatarUrl().then(setAvatarURL);
+            setIsLoaded(true);
+        };
+        getData();
+    }, [author]);
+
+    return (
+        <HStack space={1}>
+            <Skeleton borderRadius={100} width='8%' isLoaded={isLoaded}><img className='avatar' src={avatarURL === undefined ? placeholder_image : avatarURL!} alt='avatar'/></Skeleton>
+            <Skeleton.Text isLoaded={isLoaded} lines={1} width='60%' alignSelf={'center'}>
+                <Text variant='handle'>@{name}</Text>
+            </Skeleton.Text>
+        </HStack>
+    )
+}
+
+export default AuthorHandle;
