d1e85cc2aca67f198d2129c437e889e424b09362
Daniel <daniel.lomax.west@gmail.com>
2023-02-07 16:30:30 -0500

    create RouteView page with functional route feed navigation

--- src/pages/RouteView.tsx
+++ src/pages/RouteView.tsx
@@ -1,72 +1,75 @@
 import { Box, Button, Center, Flex, HStack, Text, VStack } from 'native-base';
 import { NavBar } from '../components/NavigationBar';
-import { useEffect, useState } from 'react';
-import { Route } from '../xplat/types/route';
 import { createSearchParams, useNavigate, useSearchParams } from 'react-router-dom';
 import { getRouteById } from '../xplat/api';
 import placeholder_image from '../placeholder_image.jpg';
-import { User } from '../xplat/types/user';
+import { useQuery } from 'react-query';
+import { buildRouteFetcher } from '../utils/queries';
 
 const RouteView = () => {
-    const [route, setRoute] = useState<Route>();
     const [params] = useSearchParams();
-    const [routeName, setRouteName] = useState<string>('');
-    const [setter, setSetter] = useState<string>('');
-
-    useEffect(() => {
-        if (params.has('uid')) {
-            // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-            setRoute(getRouteById(params.get('uid')!));
-        }
-    }, []);
-
-    useEffect(() => {
-        const fetchData = async () => {
-            route?.getName().then(setRouteName);
-            route?.getSetter().then(async (user: User) => {
-                setSetter(await user.getDisplayName());
-            });
-        };
-        fetchData();
-    }, [route]);
-
     const navigate = useNavigate();
-    const navToRouteFeed = () => {
-        if (route !== undefined) {
-            // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-            const exampleSearchParams = { uid: route.docRef!.id };
-            navigate({
-                pathname: '/routefeed',
-                search: `?${createSearchParams(exampleSearchParams)}`
-            });
+
+    const hasParams: boolean = params.has('uid');
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    const myUID: string = (hasParams ? params.get('uid')! : '');
+
+    // TODO: migrate this fetcher to the one in xplat and delete the one in ../utils/queries
+    const { isLoading, isError, data, error } = useQuery(
+        myUID,
+        buildRouteFetcher(getRouteById(myUID)),
+        {
+            enabled: hasParams
         }
+    );
+
+    if (!hasParams) {
+        // with our navigation this should never happen, but can never be too safe?
+        console.error('no uid params given');
+        return null;
+    }
+
+    if (isLoading) {
+        return null;
+    }
+
+    if (isError || data === undefined) {
+        console.error(error);
+        return null;
+    }
+
+    const navToRouteFeed = () => {
+        const searchParams = { uid: myUID };
+        navigate({
+            pathname: '/routefeed',
+            search: `?${createSearchParams(searchParams)}`
+        });
     };
+    const displaySetter = (data.setter?.string !== undefined ? data.setter.string : 'No assigned setter');
 
     return (
         <Box>
             <VStack>
                 <NavBar />
                 <Flex flexDir='column' justifyContent='center' top='70px' width='100%' position='fixed'>
-                    <Center><Text fontSize='2xl' bold>{routeName}</Text></Center>
+                    <Center><Text fontSize='2xl' bold>{data.name}</Text></Center>
                     <Flex flexDir='row' justifyContent='space-between' width='100%'>
                         <Button onPress={navToRouteFeed}> View Route Feed </Button>
-                        <Flex flexDir='row'>
-                            <Button> Archive </Button>
-                            <Button> Edit </Button>
-                            <Button> Delete </Button>
-                        </Flex>
+                        {/* TODO: make this button only visible for Active Routes */}
+                        <Button> Archive This Route </Button>
                     </Flex>
                     <Center><HStack>
                         {placeholder_image! && <img src={placeholder_image} className='route-avatar' alt='route' />}
                         <VStack>
-                            <Text> Top-Rope </Text>
-                            <Text> 5.10a </Text>
-                            <Text> OH </Text>
-                            <Text> Red </Text>
-                            <Text> {setter === undefined ? 'No assigned Setter' : 'Set by ' + setter} </Text>
-                            <Text> 12 sends </Text>
-                            <Text> 6 likes </Text>
-                            <Text> 4.5 stars </Text>
+                            <Text> {data.type} </Text>
+                            <Text> {data.grade} </Text>
+                            <Text> {data.naturalRules} </Text>
+                            <Text> {data.color} </Text>
+                            <Text> {displaySetter} </Text>
+                            <Text> {data.numSends} </Text>
+                            <Text> {data.numLikes} </Text>
+                            {/* TODO: make this stars field only visible to managers */}
+                            <Text> {data.stars} </Text>
                         </VStack>
                     </HStack></Center>
                 </Flex>
