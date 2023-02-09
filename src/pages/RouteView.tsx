453662240ad33e86eb49a6b556514713654875a1
Daniel <daniel.lomax.west@gmail.com>
2023-02-08 23:02:15 -0700

    add archiveRoute functionality and display changes

--- src/pages/RouteView.tsx
+++ src/pages/RouteView.tsx
@@ -5,25 +5,27 @@ import { getRouteById } from '../xplat/api';
 import placeholder_image from '../placeholder_image.jpg';
 import { useQuery } from 'react-query';
 import { buildRouteFetcher } from '../utils/queries';
+import { NaturalRules, RouteStatus, Tag } from '../xplat/types';
+import { queryClient } from '..';
 
 const RouteView = () => {
     const [params] = useSearchParams();
     const navigate = useNavigate();
 
-    const hasParams: boolean = params.has('uid');
+    const hasUID: boolean = params.has('uid');
     // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-    const myUID: string = (hasParams ? params.get('uid')! : '');
+    const myUID: string = (hasUID ? params.get('uid')! : '');
 
     // TODO: migrate this fetcher to the one in xplat and delete the one in ../utils/queries
     const { isLoading, isError, data, error } = useQuery(
-        myUID,
+        ['routes', myUID],
         buildRouteFetcher(getRouteById(myUID)),
         {
-            enabled: hasParams
+            enabled: hasUID
         }
     );
 
-    if (!hasParams) {
+    if (!hasUID) {
         // with our navigation this should never happen, but can never be too safe?
         console.error('no uid params given');
         return null;
@@ -45,7 +47,27 @@ const RouteView = () => {
             search: `?${createSearchParams(searchParams)}`
         });
     };
-    const displaySetter = (data.setter?.string !== undefined ? data.setter.string : 'No assigned setter');
+
+    const archiveThisRoute = () => {
+        // TODO: find a nice way to have the component rerender after these async calls
+        getRouteById(myUID).upgradeStatus().then(() =>
+            queryClient.invalidateQueries({ queryKey: ['routes', myUID] })
+        );
+    };
+
+    const notAssigned = 'not assigned';
+    const displayNaturalRules = data.naturalRules ? NaturalRules[data.naturalRules] : notAssigned;
+    let displayTags = '';
+    data.tags.forEach((currTag: Tag, index: number) => {
+        if (index > 0) {
+            // comma separate tags (skip putting one before the first guy)
+            displayTags += ', ';
+        }
+        displayTags += currTag.getName();
+    });
+    if (displayTags == '') {
+        displayTags = 'none';
+    }
 
     return (
         <Box>
@@ -53,23 +75,34 @@ const RouteView = () => {
                 <NavBar />
                 <Flex flexDir='column' justifyContent='center' top='70px' width='100%' position='fixed'>
                     <Center><Text fontSize='2xl' bold>{data.name}</Text></Center>
-                    <Flex flexDir='row' justifyContent='space-between' width='100%'>
-                        <Button onPress={navToRouteFeed}> View Route Feed </Button>
+                    <Flex flexDir='row' justifyContent='center' width='100%'>
+                        <Button onPress={navToRouteFeed}>
+                            <Text variant='button'>View Route Feed</Text>
+                        </Button>
                         {/* TODO: make this button only visible for Active Routes */}
-                        <Button> Archive This Route </Button>
+                        <Button onPress={archiveThisRoute}>
+                            <Text variant='button'>Archive This Route</Text>
+                        </Button>
                     </Flex>
                     <Center><HStack>
+                        {/* TODO: make the route-avatar not take up the entire screen */}
+                        {/* <img src={data.image ?? placeholder_image} className='route-avatar' alt='route' /> */}
                         {placeholder_image! && <img src={placeholder_image} className='route-avatar' alt='route' />}
                         <VStack>
-                            <Text> {data.type} </Text>
-                            <Text> {data.grade} </Text>
-                            <Text> {data.naturalRules} </Text>
-                            <Text> {data.color} </Text>
-                            <Text> {displaySetter} </Text>
-                            <Text> {data.numSends} </Text>
-                            <Text> {data.numLikes} </Text>
+                            <Text> Status: {RouteStatus[data.status]} </Text>
+                            <Text> Type: {data.type} </Text>
+                            <Text> Color: {data.color} </Text>
+                            <Text> Grade: {data.grade} </Text>
+                            <Text> Natural Rules: {displayNaturalRules} </Text>
+                            <Text> Tags: {displayTags} </Text>
+                            <Text> Rope: {data.rope ?? notAssigned} </Text>
+                            <Text> Setter: {data.setter?.string ?? notAssigned} </Text>
+                            {/* TODO: make this a readable date format? Also it is not even accurate atm */}
+                            <Text> Date Set: {data.timestamp?.toDateString() ?? notAssigned} </Text>
+                            <Text> Sends: {data.numSends} </Text>
+                            <Text> Likes: {data.numLikes} </Text>
                             {/* TODO: make this stars field only visible to managers */}
-                            <Text> {data.stars} </Text>
+                            <Text> Rating: {data.stars ?? 5} stars </Text>
                         </VStack>
                     </HStack></Center>
                 </Flex>
