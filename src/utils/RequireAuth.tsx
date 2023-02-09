ea58fcd9a0be293b54c444d575ce2b5f8d93111e
AlexRutledge1 <31017536+AlexRutledge1@users.noreply.github.com>
2023-02-09 13:34:14 -0500

    add onAuthStateChanged observer

--- src/utils/RequireAuth.tsx
+++ src/utils/RequireAuth.tsx
@@ -1,22 +1,42 @@
 import { Navigate, Outlet, useLocation } from 'react-router-dom';
 import { auth as FirebaseAuth} from '../xplat/Firebase';
 import { useQuery } from 'react-query';
+import { queryClient } from '..';
+import { useEffect } from 'react';
 import { buildUserByIDFetcher } from './queries';
-import { UserStatus } from '../xplat/types';
+import { invalidateDocRefId, UserStatus } from '../xplat/types';
+import { Link } from 'native-base';
 
 const RequireAuth = () => {
   const location = useLocation();
+
+  useEffect( () => {
+    FirebaseAuth.beforeAuthStateChanged( (user) => {
+      if (user)
+      {
+        invalidateDocRefId(user.uid);
+      }
+    });
+    FirebaseAuth.onAuthStateChanged( () => {
+      queryClient.invalidateQueries('loggedUser');
+    });
+  }, []);
   
   if (FirebaseAuth.currentUser === null)
     return <Navigate to={'/'} state={{ from: location }} replace/>;
-  
+
   const {isLoading, isError, data} = 
-    useQuery(FirebaseAuth.currentUser.uid, buildUserByIDFetcher(FirebaseAuth.currentUser.uid));
+    useQuery(['loggedUser', FirebaseAuth.currentUser.uid], buildUserByIDFetcher(FirebaseAuth.currentUser.uid));
   
   if (isLoading)
     return <>Loading user authentication</>;
   if (isError || data === undefined)
-    return <>Error loading user authentication</>;
+    return (
+      <>
+        There was an error loading the user authentication. 
+        <Link href='/'>Click here to return to the login screen.</Link>
+      </>
+    );
 
   if (data.status < UserStatus.Employee)
   {
