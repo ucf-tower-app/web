336b1851825836282e6331259b71f9a7363644e0
AlexRutledge1 <31017536+AlexRutledge1@users.noreply.github.com>
2023-02-09 10:48:06 -0500

    Check user status before loading page

--- src/utils/RequireAuth.tsx
+++ src/utils/RequireAuth.tsx
@@ -1,6 +1,8 @@
 import { Navigate, Outlet, useLocation } from 'react-router-dom';
 import { auth as FirebaseAuth} from '../xplat/Firebase';
-
+import { useQuery } from 'react-query';
+import { buildUserByIDFetcher } from './queries';
+import { UserStatus } from '../xplat/types';
 
 const RequireAuth = () => {
   const location = useLocation();
@@ -8,6 +10,17 @@ const RequireAuth = () => {
   if (FirebaseAuth.currentUser === null)
     return <Navigate to={'/'} state={{ from: location }} replace/>;
   
+  const {isLoading, isError, data} = 
+    useQuery(FirebaseAuth.currentUser.uid, buildUserByIDFetcher(FirebaseAuth.currentUser.uid));
+  
+  if (isLoading)
+    return <>Loading user authentication</>;
+  if (isError || data === undefined)
+    return <>Error loading user authentication</>;
+
+  if (data.status < UserStatus.Employee)
+    return <>You do not have permission to access this page</>;
+
   return <Outlet/>;
 };
 
