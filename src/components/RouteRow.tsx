7a7706ff3c53c9bdbc0a59464ca67ccc4c2c843a
Daniel <daniel.lomax.west@gmail.com>
2023-02-28 17:56:28 -0500

    temporary commit for Searching

--- src/components/RouteRow.tsx
+++ src/components/RouteRow.tsx
@@ -1,41 +1,48 @@
 import { Box, Flex, Text } from 'native-base';
 import { Route } from '../xplat/types/route';
-import { useEffect, useState } from 'react';
 import { useQuery } from 'react-query';
 import { createSearchParams, useNavigate } from 'react-router-dom';
 import { Pressable } from 'react-native';
 import { buildRouteFetcher } from '../utils/queries';
 
 type Props = {
-    route: Route;
+  route: Route;
 };
 export const RouteRow = ({ route }: Props) => {
-  const [name, setName] = useState<string>('');
-  const [grade, setGrade] = useState<string>('');
-  const {isLoading, data} = useQuery(['route-row', {id: route.docRef!.id}], buildRouteFetcher(route));
+  const { isLoading, isError, error, data } = useQuery(route.docRef!.id, buildRouteFetcher(route));
 
   const navigate = useNavigate();
-  const exampleSearchParams = { uid: route.docRef!.id };
   const navToRoute = () => {
+    const searchParams = { uid: route.docRef!.id };
     navigate({
       pathname: '/route',
-      search: `?${createSearchParams(exampleSearchParams)}`
+      search: `?${createSearchParams(searchParams)}`
     });
   };
 
-  useEffect(() => {
-    if (data !== undefined ) {
-      setName(data.name);
-      setGrade(data.grade);
-    }
-  }, [data]);
+  console.log('renderin route row');
+
+  if (isLoading) {
+    return (
+      <Box width='100%'>
+        <Text>Loading...</Text>
+      </Box>
+    );
+  }
+
+  if (isError || data === undefined) {
+    console.log(error);
+    return null;
+  }
+
+  console.log('full render');
 
   return (
     <Box width='100%'>
       <Pressable onPress={navToRoute} >
         <Flex flexDirection="row" justifyContent="space-between" >
-          <Text>{name}</Text>
-          <Text>{grade}</Text>
+          <Text>{data.name}</Text>
+          <Text>{data.grade}</Text>
         </Flex>
       </Pressable>
     </Box>
--- src/components/SearchBox.tsx
+++ src/components/SearchBox.tsx
@@ -1,5 +1,11 @@
-import { Box, Button, Flex, Input, Text } from 'native-base';
-import { useState } from 'react';
+import { Box, Button, Flex, Input, Text, VStack } from 'native-base';
+import { useEffect, useState } from 'react';
+import {
+  UserSearchResult, buildUserSubstringMatcher,
+  getArchivedRoutesSubstringMatcher, getRouteByName, getUserCache
+} from '../xplat/api';
+import { Route, SubstringMatcher, User } from '../xplat/types';
+import { RouteRow } from './RouteRow';
 
 const enum SearchView {
   Users,
@@ -11,9 +17,81 @@ const SearchBox = () => {
   const [view, setView] = useState<SearchView>(SearchView.Users);
   const [inputText, setInputText] = useState<string>('');
 
+  const [gotMatchers, setGotMatchers] = useState<boolean>(false);
+
+  const [userMatcher, setUserMatcher] = useState<SubstringMatcher<UserSearchResult[]>>();
+  const [userSearchResults, setUserSearchResults] = useState<UserSearchResult[]>([]);
+
+  const [archivedRoutesMatcher, setArchivedRoutesMatcher] = useState<SubstringMatcher<string>>();
+  const [archivedRoutes, setArchivedRoutes] = useState<Route[]>([]);
+
+  useEffect(() => {
+    const fetchMatchers = async () => {
+      setUserMatcher(buildUserSubstringMatcher(await getUserCache()));
+      setArchivedRoutesMatcher(await getArchivedRoutesSubstringMatcher());
+      setGotMatchers(true);
+    };
+    if (!gotMatchers) {
+      fetchMatchers();
+    }
+  }, []);
+
+  console.log(gotMatchers);
+  console.log(archivedRoutes.length);
+
+  const updateSearchResults = async () => {
+    if (!gotMatchers) {
+      return;
+    }
+
+    if (view === SearchView.Users) {
+      // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+      setUserSearchResults(userMatcher!.getMatches(inputText));
+    }
+    else if (view === SearchView.ArchivedRoutes) {
+      // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+      const routeNames = await archivedRoutesMatcher!.getMatches(inputText);
+      console.log('routeNames (item count = ' + routeNames.length + '):');
+      routeNames.forEach((routeName: string) => console.log(routeName));
+
+      const routes: Route[] = [];
+      for (const routeName in routeNames) {
+        const currRoute: Route | undefined = await getRouteByName(routeName);
+        console.log('grabbed it async?');
+        if (currRoute !== undefined) {
+          console.log('pushin it on?');
+          routes.push(currRoute);
+        }
+      }
+      console.log('created routes array of length ' + routes.length);
+      setArchivedRoutes(routes);
+
+      console.log('just updated archived routes');
+
+
+      // setArchivedRoutesSearchResults(archivedRoutesMatcher!.getMatches(inputText));
+    }
+  };
+
+  const results =
+    view === SearchView.Users
+      ? userSearchResults.map((userSearchResult: UserSearchResult) => {
+        return (
+          <Box key={userSearchResult.user.getId()}>
+            <Text>{userSearchResult.username}</Text>
+          </Box>
+        );
+      })
+      : view === SearchView.ArchivedRoutes
+        ? archivedRoutes.map((currRoute: Route) => {
+          return (
+            <RouteRow key={currRoute.docRef!.id} route={currRoute} />
+          );
+        })
+        : [];
 
   return (
-    <Flex flexDir='column'>
+    <VStack>
       <Flex flexDir='row' justifyContent='center'>
         <Box>
           <Button
@@ -21,37 +99,44 @@ const SearchBox = () => {
             variant={view === SearchView.Users ? 'solid' : 'outline'}
             rounded="full"
           >
-            <Text variant='button'>Users</Text>
+            <Text>Users</Text>
           </Button>
         </Box >
+        {/* <Box>
+          <Button
+            onPress={() => setView(SearchView.ActiveRoutes)}
+            variant={view === SearchView.ActiveRoutes ? 'solid' : 'outline'}
+            rounded="full"
+          >
+            <Text>Activedick Routes</Text>
+          </Button>
+        </Box > */}
         <Box>
           <Button
             onPress={() => setView(SearchView.ArchivedRoutes)}
             variant={view === SearchView.ArchivedRoutes ? 'solid' : 'outline'}
             rounded="full"
           >
-            <Text variant='button'>Archived Routes</Text>
+            <Text>Archived Routes</Text>
           </Button>
         </Box >
       </Flex>
-      <Input
-        size="md"
-        variant="rounded"
-        value={inputText}
-        // InputRightElement={
-        //   <Pressable onPress={() => handleInput('')}>
-        //     <Icon as={<Feather name="x" />} size="md" color="black" mr="4" />
-        //   </Pressable>
-        // }
-        // InputLeftElement={
-        //   <Icon as={<Feather name="search" />} size="md" color="black" ml="4" />
-        // }
-        focusOutlineColor="purple.500"
-        backgroundColor="white"
-        autoCorrect={false}
-        autoComplete="off"
-      />
-    </Flex>
+      <Flex flexDir='row' justifyContent='center'>
+        <Input
+          value={inputText}
+          onChangeText={setInputText}
+          backgroundColor="white"
+          autoCorrect={false}
+          autoComplete="off"
+        />
+        <Button onPress={updateSearchResults}>
+          <Text variant='button'>Search!</Text>
+        </Button>
+      </Flex>
+      <Flex flexDir='column' alignItems='center'>
+        {results}
+      </Flex>
+    </VStack>
   );
 };
 
