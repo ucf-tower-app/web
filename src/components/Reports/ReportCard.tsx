660f6bb3288031c54257d5552e9ae6eb2fc6160c
AlexRutledge1 <31017536+AlexRutledge1@users.noreply.github.com>
2023-02-27 19:29:30 -0500

    Formatting and change placeholder modReason

--- src/components/Reports/ReportCard.tsx
+++ src/components/Reports/ReportCard.tsx
@@ -2,7 +2,14 @@ import { Post, User, Comment } from '../../xplat/types';
 import ReportedComment from './ReportedComment';
 import ReportedPost from './ReportedPost';
 import ReportedUser from './ReportedUser';
-import { ArrowForwardIcon, HStack, Text, Button, Box, Input } from 'native-base';
+import {
+  ArrowForwardIcon,
+  HStack,
+  Text,
+  Button,
+  Box,
+  Input,
+} from 'native-base';
 import { useState, useEffect, useContext } from 'react';
 import { AuthContext } from '../../utils/AuthContext';
 import { queryClient } from '../../App';
@@ -13,18 +20,26 @@ import { ModerationAction } from '../../pages/Reports';
 import AuthorHandle from '../User/AuthorHandle';
 
 /*
-  * Users can report content that they believe should not be hosted on the Tower app.
-  * Reported content gets put into a queue that employees can view to decide if any
-  * moderation actions need to be taken on the content or the auther of the content.
-  * 
-  * The ReportCard component determines the type of reported content and renders the 
-  * appropriate card.
-*/
+ * Users can report content that they believe should not be hosted on the Tower app.
+ * Reported content gets put into a queue that employees can view to decide if any
+ * moderation actions need to be taken on the content or the auther of the content.
+ *
+ * The ReportCard component determines the type of reported content and renders the
+ * appropriate card.
+ */
 
-const ReportCard = ({content, reporters}: {content: Post | Comment | User, reporters: User[]}) => {
+const ReportCard = ({
+  content,
+  reporters,
+}: {
+  content: Post | Comment | User;
+  reporters: User[];
+}) => {
   const [author, setAuthor] = useState<User | undefined>(undefined);
   const [confirmationOpen, setConfirmationOpen] = useState<boolean>(false);
-  const [moderationAction, setModerationAction] = useState<ModerationAction>(ModerationAction.None);
+  const [moderationAction, setModerationAction] = useState<ModerationAction>(
+    ModerationAction.None
+  );
   const [contentjsx, setContentjsx] = useState<JSX.Element>(<></>);
 
   const handleGetAuthor = async (content: Post | Comment) => {
@@ -32,67 +47,76 @@ const ReportCard = ({content, reporters}: {content: Post | Comment | User, repor
   };
 
   useEffect(() => {
-    if (content instanceof User)
-    {
-      setContentjsx(<ReportedUser user={content}/>);
+    if (content instanceof User) {
+      setContentjsx(<ReportedUser user={content} />);
       setAuthor(content);
-    } else
-    if (content instanceof Post)
-    {
-      setContentjsx(<ReportedPost post={content}/>);
+    } else if (content instanceof Post) {
+      setContentjsx(<ReportedPost post={content} />);
       handleGetAuthor(content);
-    } else{
-      setContentjsx(<ReportedComment comment={content}/>);
+    } else {
+      setContentjsx(<ReportedComment comment={content} />);
       handleGetAuthor(content);
     }
   }, []);
 
-  useEffect( () => {
-    if (confirmationOpen === false)
-    {
+  useEffect(() => {
+    if (confirmationOpen === false) {
       setModerationAction(ModerationAction.None);
     }
   }, [confirmationOpen]);
-    
-  
 
   return (
     <HStack justifyContent='space-evenly'>
-      {author !== undefined && <ConfirmationPopup content={content} open={confirmationOpen} 
-        setOpen={setConfirmationOpen} type={moderationAction} author={author}/>}
+      {author !== undefined && (
+        <ConfirmationPopup
+          content={content}
+          open={confirmationOpen}
+          setOpen={setConfirmationOpen}
+          type={moderationAction}
+          author={author}
+        />
+      )}
       {contentjsx}
-      <ArrowForwardIcon size='lg' alignSelf='center' color='white'/>
+      <ArrowForwardIcon size='lg' alignSelf='center' color='white' />
       <Text variant='body' alignSelf='center' color='white'>
         This was reported by{' '}
         <Popup
           position='bottom center'
           on={['hover', 'focus']}
-          trigger={<text className='reporter-handle'>{reporters.length} user{reporters.length > 1 && 's'}.</text>}
+          trigger={
+            <text className='reporter-handle'>
+              {reporters.length} user{reporters.length > 1 && 's'}.
+            </text>
+          }
         >
           {reporters.map((reporter) => {
-            return (
-              <AuthorHandle author={reporter} key={reporter.getId()}/>
-            );
+            return <AuthorHandle author={reporter} key={reporter.getId()} />;
           })}
         </Popup>
       </Text>
       <HStack alignSelf='center' space={1}>
-        <Button onPress={() => {
-          setModerationAction(ModerationAction.Absolve);
-          setConfirmationOpen(true);
-        }}>
-          <Text variant='button' >Absolve</Text>
+        <Button
+          onPress={() => {
+            setModerationAction(ModerationAction.Absolve);
+            setConfirmationOpen(true);
+          }}
+        >
+          <Text variant='button'>Absolve</Text>
         </Button>
-        <Button onPress={() => {
-          setModerationAction(ModerationAction.Delete);
-          setConfirmationOpen(true);
-        }}>
+        <Button
+          onPress={() => {
+            setModerationAction(ModerationAction.Delete);
+            setConfirmationOpen(true);
+          }}
+        >
           <Text variant='button'>Delete</Text>
         </Button>
-        <Button onPress={() => {
-          setModerationAction(ModerationAction.Ban);
-          setConfirmationOpen(true);
-        }}>
+        <Button
+          onPress={() => {
+            setModerationAction(ModerationAction.Ban);
+            setConfirmationOpen(true);
+          }}
+        >
           <Text variant='button'>Ban</Text>
         </Button>
       </HStack>
@@ -101,31 +125,34 @@ const ReportCard = ({content, reporters}: {content: Post | Comment | User, repor
 };
 
 type ConfirmationPopupProps = {
-  content: Post | Comment | User, 
-  author: User,
-  open: boolean, 
-  setOpen: (arg0: boolean) => void, 
-  type: ModerationAction
+  content: Post | Comment | User;
+  author: User;
+  open: boolean;
+  setOpen: (arg0: boolean) => void;
+  type: ModerationAction;
 };
 
-const ConfirmationPopup = ({content, author, open, setOpen,  type}: ConfirmationPopupProps) => {
+const ConfirmationPopup = ({
+  content,
+  author,
+  open,
+  setOpen,
+  type,
+}: ConfirmationPopupProps) => {
   const [modReason, setModReason] = useState('');
   const [password, setPassword] = useState('');
   const authContext = useContext(AuthContext);
 
-  useEffect( () => {
-    if (open === false)
-    {
+  useEffect(() => {
+    if (open === false) {
       setPassword('');
       setModReason('');
     }
   }, [open]);
 
-  if (type === ModerationAction.None)
-    return <></>;
-  
-  if (authContext.user === null)
-    return <></>;
+  if (type === ModerationAction.None) return <></>;
+
+  if (authContext.user === null) return <></>;
 
   return (
     <Popup modal open={open} onClose={() => setOpen(false)}>
@@ -134,52 +161,80 @@ const ConfirmationPopup = ({content, author, open, setOpen,  type}: Confirmation
           Confirm
         </Text>
         <Text variant='body' color='black' alignSelf='center'>
-          Are you sure you want to {
-            type === ModerationAction.Absolve ? 'absolve this content' :
-              type === ModerationAction.Delete ? 'delete this content' :
-                'ban this user'
-          }? <Text variant='body' color='black' bold>This cannot be undone</Text>
+          Are you sure you want to{' '}
+          {type === ModerationAction.Absolve
+            ? 'absolve this content'
+            : type === ModerationAction.Delete
+              ? 'delete this content'
+              : 'ban this user'}
+          ?{' '}
+          <Text variant='body' color='black' bold>
+            This cannot be undone
+          </Text>
         </Text>
-        {(type === ModerationAction.Ban || type === ModerationAction.Delete) &&
-          <Input onChangeText={setModReason} multiline w='50%' alignSelf='center' placeholder='Reason for ban' mt={4}/>
-        }
-        {
-          type === ModerationAction.Ban && 
-          <Input type='password' placeholder='Password'
-            alignSelf='center' w='50%' marginTop={1} onChangeText={setPassword}/>
-        }
+        {(type === ModerationAction.Ban ||
+          type === ModerationAction.Delete) && (
+          <Input
+            onChangeText={setModReason}
+            multiline
+            w='50%'
+            alignSelf='center'
+            placeholder={
+              type === ModerationAction.Ban
+                ? 'Reason for ban'
+                : 'Reason for deleting this content'
+            }
+            mt={4}
+          />
+        )}
+        {type === ModerationAction.Ban && (
+          <Input
+            type='password'
+            placeholder='Password'
+            alignSelf='center'
+            w='50%'
+            marginTop={1}
+            onChangeText={setPassword}
+          />
+        )}
         <HStack justifyContent='center' mt={4}>
-          <Button onPress={() => {
-            setOpen(false);
-          }}>
+          <Button
+            onPress={() => {
+              setOpen(false);
+            }}
+          >
             <Text variant='button'>Cancel</Text>
           </Button>
-          <Button onPress={() => {
-            if (type === ModerationAction.Absolve)
-            {
-              authContext.user?.userObject.clearAllReports(content).then(() => {
-                queryClient.invalidateQueries({queryKey: ['reports']});
-                setOpen(false);
-              });
-            }
-            else
-            if (type === ModerationAction.Ban)
-            {
-              authContext.user?.userObject.banUser(author, modReason, password).then( () => {
-                queryClient.invalidateQueries({queryKey: ['reports']});
-                setOpen(false);
-              }).catch( (reason) => {
-                alert(reason);
-              });
-            }
-            else // Delete
-            {
-              authContext.user?.userObject.deleteReportedContent(content, modReason).then(() => {
-                queryClient.invalidateQueries({queryKey: ['reports']});
-                setOpen(false);
-              });
-            }
-          }}>
+          <Button
+            onPress={() => {
+              if (type === ModerationAction.Absolve) {
+                authContext.user?.userObject
+                  .clearAllReports(content)
+                  .then(() => {
+                    queryClient.invalidateQueries({ queryKey: ['reports'] });
+                    setOpen(false);
+                  });
+              } else if (type === ModerationAction.Ban) {
+                authContext.user?.userObject
+                  .banUser(author, modReason, password)
+                  .then(() => {
+                    queryClient.invalidateQueries({ queryKey: ['reports'] });
+                    setOpen(false);
+                  })
+                  .catch((reason) => {
+                    alert(reason);
+                  });
+              } // Delete
+              else {
+                authContext.user?.userObject
+                  .deleteReportedContent(content, modReason)
+                  .then(() => {
+                    queryClient.invalidateQueries({ queryKey: ['reports'] });
+                    setOpen(false);
+                  });
+              }
+            }}
+          >
             <Text variant='button'>Confirm</Text>
           </Button>
         </HStack>
