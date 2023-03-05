101001162d17e295d998d94a8e311f16a4cde36f
AlexRutledge1 <31017536+AlexRutledge1@users.noreply.github.com>
2023-03-05 10:51:42 -0500

    Add clicking functionality to nav to profile

--- src/components/Reports/ReportedUser.tsx
+++ src/components/Reports/ReportedUser.tsx
@@ -1,9 +1,15 @@
 import { User } from '../../xplat/types';
 import { useQuery } from 'react-query';
-import { Box, HStack, Text } from 'native-base';
+import { Box, HStack, Text, Pressable } from 'native-base';
+import { useNavigate, createSearchParams } from 'react-router-dom';
 
 const ReportedUser = ({user}: {user: User}) => {
   const {data, isLoading, isError} = useQuery(user.getId(), () => user.fetch());
+  const navigate = useNavigate();
+
+  const navigateToProfile = () => {
+    navigate(`/profile?${createSearchParams({ uid: user.docRef!.id })}`);
+  };
 
   if (isLoading)
   {
@@ -28,14 +34,15 @@ const ReportedUser = ({user}: {user: User}) => {
   }
 
   return (
-    <Box bg='primary.200' p={1} rounded='md' w='40%' borderColor='black' borderWidth='1'>
+    <Pressable bg='primary.200' p={1} rounded='md' w='40%' borderColor='black' borderWidth='1'
+      onPress={() => navigateToProfile()}>
       <HStack>
         <img src={data.avatarUrl} className='avatar' alt='reported user avatar'/>
         <Text variant='displayname'>{data.displayName}</Text>
         <Text variant='handle'>@{data.username}</Text>
       </HStack>
       {data.bio}
-    </Box>
+    </Pressable>
   );
 };
 
