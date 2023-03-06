cbf75ec25dd785a5984c9a18c7db24382aeb4180
Henry Graves <henrygraves11@yahoo.com>
2023-03-06 15:43:19 -0500

    fix up component logic, format, get password, update status on backend

--- src/components/Profile/ProfileBanner.tsx
+++ src/components/Profile/ProfileBanner.tsx
@@ -13,14 +13,19 @@ const ProfileBanner = ({user}: {user: FetchedUserProfile | undefined}) => {
   const authContext = useContext(AuthContext);
   const [newPermission, setNewPermission] = useState<number>(0);
   const [actionDescription, setActionDescription] = useState<string>('');
+  const [passwordCheck, setPasswordCheck] = useState<string>('');
 
   const handleEditPermission = () => {
     setEditPermissionModal(false);
-    console.log('will change permission to ' + newPermission);
+
+    if (user != undefined) {
+      console.log('will change permission to ' + UserStatus[newPermission]);
+      authContext.user?.userObject.editOtherStatus(passwordCheck, user?.userObject, newPermission, actionDescription);
+    } 
   };
 
-  const managerSwap = () => {
-    console.log('Will make this user manager and remove manager status from current manager');
+  const getStatusTitle = (status: number) => {
+    return UserStatus[status];
   };
 
   if (user === undefined)
@@ -47,12 +52,8 @@ const ProfileBanner = ({user}: {user: FetchedUserProfile | undefined}) => {
         onClose={() => setEditPermissionModal(false)} modal>
         <Box p={4} w='100%' h='100%' borderRadius='sm'>
           <Text alignSelf='center' bold fontSize='lg'>Edit user permission level</Text>
-          { user.status == UserStatus.Employee && authContext.user.status >= UserStatus.Manager
-            && 
-            <Button onPress={managerSwap} marginTop='1' m='3'>
-              <Text variant='button'>Make Manager</Text>
-            </Button>
-          }
+          <Text alignSelf='center' fontSize='sm' marginBottom='1'>
+            Current permission level: {getStatusTitle(user.status)}</Text>
           <Radio.Group name='Permission' value={promoteOrDemote} 
             onChange={(nextValue) => setPromoteOrDemote(nextValue)}>
             <Radio value='promote'>Promote</Radio>
@@ -63,25 +64,35 @@ const ProfileBanner = ({user}: {user: FetchedUserProfile | undefined}) => {
             promoteOrDemote === 'promote' && user.status > UserStatus.Banned ?
               <Select placeholder='Select permission level' 
                 onValueChange={(valueString) => setNewPermission(parseInt(valueString))}>
+                {user.status < UserStatus.Manager && authContext.user.status >= UserStatus.Manager &&
+                  <Select.Item label='Manager' value='4' />}
                 {user.status < UserStatus.Employee && authContext.user.status >= UserStatus.Manager &&
-                <Select.Item label='Employee' value='3' />}
-                {user.status < UserStatus.Approved && <Select.Item label='Approved (can post)' value='2' />}
-                {user.status < UserStatus.Verified && 
+                  <Select.Item label='Employee' value='3' />}
+                {user.status < UserStatus.Approved &&
+                  <Select.Item label='Approved (can post)' value='2' />}
+                {user.status < UserStatus.Verified &&
                   <Select.Item label='Verified (can login, cannot post)' value='1' />}
-                {user.status < UserStatus.Unverified && <Select.Item label='Unverified (cannot login)' value='0' />}
+                {user.status < UserStatus.Unverified &&
+                  <Select.Item label='Unverified (cannot login)' value='0' />}
               </Select>
               :
               /*Demote the user profile*/
               <Select placeholder='Select permission level'
                 onValueChange={(valueString) => setNewPermission(parseInt(valueString))}>
-                {user.status > UserStatus.Approved && <Select.Item label='Approved (can post)' value='2' />}
-                {user.status > UserStatus.Verified && 
+                {user.status > UserStatus.Employee && authContext.user.status >= UserStatus.Manager &&
+                  <Select.Item label='Employee' value='3' />}
+                {user.status > UserStatus.Approved &&
+                  <Select.Item label='Approved (can post)' value='2' />}
+                {user.status > UserStatus.Verified &&
                   <Select.Item label='Verified (can login, cannot post)' value='1' />}
-                {user.status > UserStatus.Unverified && <Select.Item label='Unverified (cannot login)' value='0' />}
+                {user.status > UserStatus.Unverified &&
+                  <Select.Item label='Unverified (cannot login)' value='0' />}
               </Select>
           }
           <Input type='text' multiline placeholder='Reason for action' marginY='1' numberOfLines={3}
             onChangeText={(text) => setActionDescription(text)} />
+          <Input type='text' multiline placeholder='Enter your password' marginY='1' numberOfLines={1}
+            onChangeText={(text) => setPasswordCheck(text)} />
           <HStack>
             <Button onPress={() => setEditPermissionModal(false)} marginTop='1'>
               <Text variant='button'>Cancel</Text>
